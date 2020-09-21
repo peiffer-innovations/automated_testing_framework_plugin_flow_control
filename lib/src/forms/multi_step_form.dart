7f378be73d1875830a1ed66152f35dbd85b44d69
limonadev <limonadev@whiteleaf.dev>
2020-09-21 17:04:02 -0500

    Added list of steps in multi_step form

--- lib/src/forms/multi_step_form.dart
+++ lib/src/forms/multi_step_form.dart
@@ -11,10 +11,26 @@ class MultiStepForm extends TestStepForm {
     bool minify = false,
   }) {
     // TODO: MultiStep Form
-    //var translator = Translator.of(context);
+    /// If this line is removed, [values] will still have a reference to the
+    /// original [steps] List, so modifying that here will modify it in the
+    /// [TestStep] without confirmation.
+    values['steps'] = List.from(values['steps'] ?? []);
     return Column(
       children: [
-        Text('TODO'),
+        if (minify != true)
+          buildHelpSection(
+            context,
+            TestFlowControlTranslations.atf_flow_help_multi_step,
+            minify: minify,
+          ),
+        buildValuesSection(
+          context,
+          [
+            _StepsEditor(
+              values: values,
+            )
+          ],
+        ),
       ],
     );
   }
@@ -25,4 +41,76 @@ class MultiStepForm extends TestStepForm {
   @override
   TranslationEntry get title =>
       TestFlowControlTranslations.atf_flow_title_multi_step;
+}
+
+class _StepsEditor extends StatefulWidget {
+  _StepsEditor({@required this.values});
+
+  final Map<String, dynamic> values;
+
+  @override
+  _StepsEditorState createState() => _StepsEditorState();
+}
+
+class _StepsEditorState extends State<_StepsEditor> {
+  List<TestStep> steps = [];
+  Translator translator;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.values['steps'] ??= [];
+    steps = (widget.values['steps'] as List)
+        .map((rawStep) => TestStep.fromDynamic(rawStep))
+        .toList();
+    translator = Translator.of(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            itemCount: steps.length,
+            itemBuilder: (context, index) {
+              return TestStepPicker(
+                label: translator.translate(
+                  TestFlowControlTranslations.atf_flow_form_inner_step,
+                ),
+                onStepChanged: (step) {
+                  widget.values['steps'][index] = step?.toJson();
+                  setState(() {
+                    steps[index] = step;
+                  });
+                },
+                step: steps[index],
+              );
+            },
+          ),
+        ),
+        Divider(
+          color: IconTheme.of(context).color,
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              /*steps.add(
+                TestStepPicker(
+                  label: translator.translate(
+                    TestTranslations.atf_no_test_step,
+                  ),
+                  onStepChanged: (step) {},
+                  step: TestStep.fromDynamic(
+                      widget.values['steps'][steps.length]),
+                ),
+              );*/
+            });
+          },
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
+  }
 }
