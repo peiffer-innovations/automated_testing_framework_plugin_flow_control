fa0f22f21b58bbd119105fdd4d89788bf7c55b6c
limonadev <limonadev@whiteleaf.dev>
2020-09-22 10:55:13 -0500

    Added steps key

--- lib/src/test_flow_control_helper.dart
+++ lib/src/test_flow_control_helper.dart
@@ -129,7 +129,7 @@ class TestFlowControlHelper {
           form: MultiStepForm(),
           help: TestFlowControlTranslations.atf_flow_help_multi_step,
           id: 'multi_step',
-          keys: const {},
+          keys: const {'steps'},
           quickAddValues: null,
           title: TestFlowControlTranslations.atf_flow_title_multi_step,
           type: null,
