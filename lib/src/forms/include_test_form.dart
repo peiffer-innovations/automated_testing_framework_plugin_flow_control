55ef554ada24d4a9430a66702300b3dd1cf9b8af
limonadev <limonadev@whiteleaf.dev>
2020-09-28 15:33:31 -0500

    Added suite form and typeahead test with validation

--- lib/src/forms/include_test_form.dart
+++ lib/src/forms/include_test_form.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
-
 import 'package:automated_testing_framework/automated_testing_framework.dart';
 import 'package:automated_testing_framework_plugin_flow_control/src/test_flow_control_translations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:form_validation/form_validation.dart';
 import 'package:static_translations/static_translations.dart';
 
 class IncludeTestForm extends TestStepForm {
@@ -30,7 +29,9 @@ class IncludeTestForm extends TestStepForm {
         buildValuesSection(
           context,
           [
-            _TestEditor(),
+            _TestEditor(
+              values: values,
+            ),
           ],
         ),
       ],
@@ -39,37 +40,81 @@ class IncludeTestForm extends TestStepForm {
 }
 
 class _TestEditor extends StatefulWidget {
+  _TestEditor({@required this.values});
+
+  final Map<String, dynamic> values;
+
   @override
   _TestEditorState createState() => _TestEditorState();
 }
 
 class _TestEditorState extends State<_TestEditor> {
-  List<String> availableTests = [];
-  bool testsWereLoaded = false;
+  List<PendingTest> availableTests = [];
+  TextEditingController testController = TextEditingController();
+  TextEditingController suiteController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _getAvailableTests();
+    suiteController.text = widget.values['suiteName'];
+    suiteController.addListener(() {
+      _updateValues(
+        widget.values,
+        suiteName: suiteController.text,
+      );
+    });
+    testController.text = widget.values['testName'];
+    testController.addListener(() {
+      _updateValues(
+        widget.values,
+        testName: testController.text,
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return TypeAheadField(
-      textFieldConfiguration: TextFieldConfiguration(
-        enabled: testsWereLoaded,
-      ),
-      suggestionsCallback: (pattern) {
-        return _getSuggestionsFrom(pattern, 3);
-      },
-      itemBuilder: (context, suggestion) {
-        return ListTile(
-          title: Text(suggestion),
-        );
-      },
-      onSuggestionSelected: (suggestion) {
-        //print(suggestion);
-      },
+    return Column(
+      children: [
+        TextFormField(
+          controller: suiteController,
+          decoration: InputDecoration(
+            labelText: 'Suite Name', //TODO: Use translations
+          ),
+        ),
+        TypeAheadFormField(
+          autovalidate: true,
+          hideOnEmpty: true,
+          itemBuilder: (context, test) {
+            return ListTile(
+              //TODO: Use translations
+              title: Text('Test: ${test.name}'),
+              subtitle: Text('Suite: ${test.suiteName ?? ''}'),
+            );
+          },
+          onSuggestionSelected: (test) {
+            suiteController.text = test.suiteName;
+            testController.text = test.name;
+          },
+          suggestionsCallback: (testName) {
+            return _getSuggestionsFrom(testName: testName);
+          },
+          textFieldConfiguration: TextFieldConfiguration(
+            controller: testController,
+            decoration: InputDecoration(
+              labelText: 'Test Name', //TODO: Use translations
+            ),
+          ),
+          validator: (testName) => Validator(
+            validators: [RequiredValidator()],
+          ).validate(
+            context: context,
+            label: 'Test Name', //TODO: Use translations
+            value: testName,
+          ),
+        ),
+      ],
     );
   }
 
@@ -77,22 +122,31 @@ class _TestEditorState extends State<_TestEditor> {
     var runner = TestRunner.of(context);
     var tests = await runner.controller.loadTests(context);
     tests.forEach(
-      (test) => availableTests.add(test.name),
+      (test) => availableTests.add(test),
     );
-    setState(() {
-      testsWereLoaded = true;
-    });
-    //print(availableTests);
   }
 
-  List<String> _getSuggestionsFrom(String pattern, int limit) {
+  List<PendingTest> _getSuggestionsFrom({
+    int limit = 3,
+    @required String testName,
+  }) {
     var result = availableTests
         .where(
-          (test) => test.toLowerCase().contains(pattern),
+          (test) => test.name.toLowerCase().contains(testName.toLowerCase()),
         )
         .take(limit)
         .toList();
-
     return result;
+  }
+
+  void _updateValues(
+    Map<String, dynamic> values, {
+    String suiteName,
+    String testName,
+  }) {
+    values['suiteName'] =
+        suiteName?.isNotEmpty != true ? values['suiteName'] : suiteName;
+    values['testName'] =
+        testName?.isNotEmpty != true ? values['testName'] : testName;
   }
 }
